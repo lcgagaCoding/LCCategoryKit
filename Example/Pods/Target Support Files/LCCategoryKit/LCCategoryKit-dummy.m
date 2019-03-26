1eb62513117b7e3e5ddc4d96a40b7ab0b5087bfe
刘成 <239668571@qq.com>
2019-03-26 11:33:16 +0800

    xxx

---
+++ Example/Pods/Target Support Files/LCCategoryKit/LCCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LCCategoryKit : NSObject
+@end
+@implementation PodsDummy_LCCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/LCCategoryKit/LCCategoryKit-umbrella.h
@@ -0,0 +1,36 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSObject+UnRecognizedSelHandler.h"
+#import "NSArray+LCProtect.h"
+#import "NSDate+SCAddition.h"
+#import "NSString+LCAttriButedCreate.h"
+#import "NSString+LCDateSwitch.h"
+#import "NSString+LCIdCardInfo.h"
+#import "NSString+STRegex.h"
+#import "UIFont+LCAdjustFont.h"
+#import "UINavigationController+Cloudox.h"
+#import "UIView+Extension.h"
+#import "UIViewController+Cloudox.h"
+#import "MBProgressHUD+LCHud.h"
+#import "CALayer+LCLayerColor.h"
+#import "LCUICreate.h"
+#import "UIButton+LCCreate.h"
+#import "UIColor+LCImageCreate.h"
+#import "UIImageView+LCCreate.h"
+#import "UILabel+LCCreate.h"
+#import "UIView+LCCreate.h"
+#import "YCCategory.h"
+
+FOUNDATION_EXPORT double LCCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char LCCategoryKitVersionString[];
+
